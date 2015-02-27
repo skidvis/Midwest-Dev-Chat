14c2da4d35bdb261e5b747558651848609619271
Skid Vis <skid.vis@agapered.com>
2015-02-27 16:46:39 -0600

    switch to postgres.

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150227163831) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "members", force: :cascade do |t|
     t.string   "slack_id"
     t.string   "name"
