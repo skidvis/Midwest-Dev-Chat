2290edb4e74350fefdf35ec0bb9fc33c44a4c21a
Skid Vis <skid.vis@agapered.com>
2015-02-27 18:04:58 -0600

    fix mailer template issue.

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,3 @@
 class ApplicationMailer < ActionMailer::Base
   default from: "info@midwestdevchat.com"
-  layout 'mailer'
 end
--- config/deploy.rb
+++ config/deploy.rb
@@ -65,7 +65,7 @@ task :deploy => :environment do
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
     invoke :'bundle:install'
-    invoke :'rails:db_migrate'
+    invoke :'rails:db_migrate:force'
     invoke :'rails:assets_precompile'
     invoke :'deploy:cleanup'
 
