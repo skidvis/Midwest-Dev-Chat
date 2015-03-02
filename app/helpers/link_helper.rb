ba5ab1a218b3b249b4183bbaf24b56b7dba71257
Skid Vis <skid.vis@agapered.com>
2015-03-02 16:43:12 -0600

    Remove usernames. Remove access to channels other than #general.

--- app/helpers/link_helper.rb
+++ app/helpers/link_helper.rb
@@ -8,7 +8,7 @@ module LinkHelper
     message = find_code_in_text(message)
     message = find_emojis_in_text(message)
 
-    "<span class='date'>#{message_date}</span> <span class='member' style='color: ##{user.color}'>#{user.name}:</span> <span>#{message}</span>"
+    "<span class='date'>#{message_date}</span> <span class='member' style='color: ##{user.color}'>user#{user.id}:</span> <span>#{message}</span>"
   end
 
   def self.find_members_in_text(message)
@@ -16,7 +16,7 @@ module LinkHelper
       found_user_id = Regexp.last_match[1].split('|').first
       if found_user_id.present?
         found_user = Member.find_by_slack_id(found_user_id) || add_new_user(found_user_id)
-        "<span class='member' style='color:##{found_user.color}'>@#{found_user.name}</span>"
+        "<span class='member' style='color:##{found_user.color}'>@user#{found_user.id}</span>"
       end
     end
   end
