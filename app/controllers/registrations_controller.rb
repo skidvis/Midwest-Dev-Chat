f6424fc149df3ae715100d8123dbc9c0b4275821
Skid Vis <skid@skidvis.com>
2017-08-20 13:46:04 -0500

    add slackhandle to new user controller

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -3,7 +3,7 @@ class RegistrationsController < Devise::RegistrationsController
     private
   
     def sign_up_params
-      params.require(:user).permit(:fullname, :email, :password, :password_confirmation)
+      params.require(:user).permit(:fullname, :slackhandle,:email, :password, :password_confirmation)
     end
   
     def account_update_params
