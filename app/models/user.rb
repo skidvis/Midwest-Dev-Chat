ce09263b99eb24a5e6903bdf06687887381bed2a
Skid Vis <skid@skidvis.com>
2017-08-20 13:52:11 -0500

    add uniqueness contraint on slackhandle

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-  validates :slackhandle, presence: true
+  validates :slackhandle, presence: true, uniqueness: { scope: :slackhandle,  message: "already exists" }
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
