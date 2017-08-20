8860ff648f3d51bc0357c3e6ed153e86746f9927
Skid Vis <skid@skidvis.com>
2017-08-20 17:46:36 -0500

    change post_to_slack time to after midnight.

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,7 +1,7 @@
 set :environment, 'production'
 set :output, {:error => 'log/cron_error_log.log', :standard => 'log/cron_log.log'}
 
-every 1.day, :at => '11:59 pm' do
+every 1.day, :at => '12:01am' do
   rake 'slack:update_channels'
   rake 'careers:update_jobs'
   rake 'careers:post_to_slack'
